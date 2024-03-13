6c15c4de924a5f94525cb0f522695167fa9e62b7
Aflaungos <vimavick@gmail.com>
2024-03-13 01:08:46 -0300

    Fix errors

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
+<<<<<<< HEAD
 export PROJECT_NAME=a42xq
 mkdir out
 
@@ -13,3 +14,13 @@ make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUIL
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+=======
+export PROJECT_NAME=m23xq
+export CLANG="${HOME}/linux-x86-main/clang-r487747c/bin"
+export PATH="$CLANG:$PATH"
+
+make O=out ARCH=arm64 SUBARCH=arm64 CC=clang LLVM_IAS=1 LLVM=1 vendor/m23xq_eur_open_defconfig
+make O=out ARCH=arm64 SUBARCH=arm64 CC=clang LLVM_IAS=1 LLVM=1
+
+$(pwd)/tools/mkdtimg create $(pwd)/out/arch/arm64/boot/dtbo.img --page_size=4096 $(find out/arch/arm64/boot/dts/samsung/m23/m23xq/ -name *.dtbo)
+>>>>>>> f0e6528e22a4... Fix warning: passing 8-byte aligned argument to 32-byte aligned parameter 2 of 'smp_call_function_single_async'
