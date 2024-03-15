29d24ff0bb8361065b5c44926defb887a2390d87
Aflaungos <vimavick@gmail.com>
2024-03-15 00:42:28 -0300

    Revert "Merge tag 'v4.19.219' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into baseline-r"
    This reverts commit 45f14188c5b89f927cc5b1a953dd46a24bb6e4c1.

--- drivers/hid/wacom_wac.h
+++ drivers/hid/wacom_wac.h
@@ -302,7 +302,6 @@ struct hid_data {
 	bool tipswitch;
 	bool barrelswitch;
 	bool barrelswitch2;
-	bool confidence;
 	int x;
 	int y;
 	int pressure;
--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -141,7 +141,7 @@ static int ip6_finish_output(struct net *net, struct sock *sk, struct sk_buff *s
 #if defined(CONFIG_NETFILTER) && defined(CONFIG_XFRM)
 	/* Policy lookup after SNAT yielded a new policy */
 	if (skb_dst(skb)->xfrm) {
-		IP6CB(skb)->flags |= IP6SKB_REROUTED;
+		IPCB(skb)->flags |= IPSKB_REROUTED;
 		return dst_output(net, sk, skb);
 	}
 #endif
